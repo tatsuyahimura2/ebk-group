014b7735486b3a0cd54d9053a72c85042d373d08
Ikhmalina <110797264+Ikhmalina@users.noreply.github.com>
2023-01-06 09:26:17 +0800

    Update dashboard.blade.php
    update cover

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-black-800 leading-tight">
-            {{ __('Sistem eBK') }}
+            {{ __('SISTEM e-KEBERHASILAN') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                 <p align=center><img src="./storage/ebk/laptop.jpg" alt height="30%" width="60%"><br>
-                 <p align=justify> 1. Garis Panduan Penilaian Komponen Keberhasilan (GPPKK) menerangkan secara khusus terma, syarat, kaedah pelaksanaan penilaian keberhasilan. Penilaian komponen
-                    keberhasilan merupakan sebahagian daripada PBPPP. Jumlah keseluruhan markah untuk penilaian keberhasilan ialah 100%. Walau bagaimanapun dalam penilaian
-                    keseluruhan PBPPP, penilaian keberhasilan menyumbang wajaran sebanyak 10%.<br><br>
+    <p align=center><img src="./storage/ebk/cover.png" alt height="80%" width="85%">
 
-                    2. PBPPP merupakan penilaian hasil kerja utama outcome/ output yang memberi impak berdasarkan tugas dan tempat bertugas. Asas penilaian keberhasilan
-                    berfokuskan kepada peningkatan atau pencapaian. Ketiga-tiga elemen berikut perlu dipertimbangkan secara profesional semasa membuat penilaian sasaran keberhasilan
-                    yang telah ditetapkan iaitu:<br><br>
-                    a) usaha; dan/ atau <br>
-                    - inisiatif dalam meningkatkan keberhasilan<br><br>
-                    b) kesungguhan; dan/ atau<br>
-                   - berusaha dengan penuh minat, tekun dan seikhlas hati<br><br>
-                    c) keberkesanan tugas<br>
-                   - hasil kerja yang memberi impak kepada organisasi<br><br>
-
-
-                </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
